933c0d3303fd502c1f19c46417eedccaa8ddf6ad
Dominik Sander <git@dsander.de>
2017-05-07 16:41:14 +0200

    Move .git directory of test repository

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,9 +10,9 @@ All notable changes to this project will be documented in this file.
 
 ### Fixed
 
-## [0.1.0] - 2017-05-06
+## [0.1.0] - 2017-05-07
 
 - First release
 
 [Unreleased]: https://github.com/dsander/pronto-simplecov/compare/v0.1.0...HEAD
-[0.1.0]: https://github.com/dsander/pronto-simplecov/compare/8ab34227842f0ff3915b84c9f3cc6bdaf19e7cf4...v0.1.0
+[0.1.0]: https://github.com/dsander/pronto-simplecov/compare/e020a804f16c5df8f1d45d1f921955928a669dd6...v0.1.0
--- spec/pronto/pronto_simplecov/runner_spec.rb
+++ spec/pronto/pronto_simplecov/runner_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
-
+require 'pp'
 describe Pronto::ProntoSimplecov::Runner do
   let(:runner) { Pronto::ProntoSimplecov::Runner.new(patches) }
   let(:patches) { [] }
@@ -9,7 +9,7 @@ describe Pronto::ProntoSimplecov::Runner do
   end
 
   context 'with patches' do
-    let(:patches) {  Pronto::Git::Repository.new('./spec/data/git').diff('master') }
+    let(:patches) {  Pronto::Git::Repository.new('./spec/data/test').diff('master') }
 
     it 'returns nothing when no coverage information is present' do
       expect(SimpleCov::ResultMerger).to receive_message_chain(:merged_result, :files).and_return({})
@@ -17,20 +17,20 @@ describe Pronto::ProntoSimplecov::Runner do
     end
 
     it 'returns nothing when unchanged lines are uncovered' do
-      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/git/test.rb'), [0, 0, 0, 1, 1, 1, 1, 1, 1])]
+      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), [0, 0, 0, 1, 1, 1, 1, 1, 1])]
       expect(SimpleCov::ResultMerger).to receive_message_chain(:merged_result, :files).and_return(files)
       expect(runner.run).to eq([])
     end
 
     it 'only creates messages for uncovered lines' do
-      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/git/test.rb'), [1, 1, 1, 1, 1, 1, 0, 1, 1])]
+      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), [1, 1, 1, 1, 1, 1, 0, 1, 1])]
       expect(SimpleCov::ResultMerger).to receive_message_chain(:merged_result, :files).and_return(files)
       expect(runner.run.length).to eq(1)
       expect(runner.run.first.msg).to eq('This change has no test coverage')
     end
 
     it 'it irgnores empt lines' do
-      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/git/test.rb'), [1, 1, 1, 1, nil, 1, 1, 1, 1])]
+      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), [1, 1, 1, 1, nil, 1, 1, 1, 1])]
       expect(SimpleCov::ResultMerger).to receive_message_chain(:merged_result, :files).and_return(files)
       expect(runner.run).to eq([])
     end
