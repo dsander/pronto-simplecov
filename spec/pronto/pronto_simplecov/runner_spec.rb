42ef2b5c4f008425be12da0ae62050eedcc01134
Dominik Sander <git@dsander.de>
2021-01-26 21:52:27 +0100

    Adjust specs to return values of new simplecov
    The gem still works with simplecov 0.1x and 0.2x, but since we are
    mocking internals we are using the new format to that CI works.

--- spec/pronto/pronto_simplecov/runner_spec.rb
+++ spec/pronto/pronto_simplecov/runner_spec.rb
@@ -17,20 +17,20 @@ describe Pronto::ProntoSimplecov::Runner do
     end
 
     it 'returns nothing when unchanged lines are uncovered' do
-      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), [0, 0, 0, 1, 1, 1, 1, 1, 1])]
+      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), {"lines" => [0, 0, 0, 1, 1, 1, 1, 1, 1]})]
       expect(SimpleCov::ResultMerger).to receive_message_chain(:merged_result, :files).and_return(files)
       expect(runner.run).to eq([])
     end
 
     it 'only creates messages for uncovered lines' do
-      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), [1, 1, 1, 1, 1, 1, 0, 1, 1])]
+      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), {"lines" => [1, 1, 1, 1, 1, 1, 0, 1, 1]})]
       expect(SimpleCov::ResultMerger).to receive_message_chain(:merged_result, :files).and_return(files)
       expect(runner.run.length).to eq(1)
       expect(runner.run.first.msg).to eq('This change has no test coverage')
     end
 
     it 'it irgnores empt lines' do
-      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), [1, 1, 1, 1, nil, 1, 1, 1, 1])]
+      files = [SimpleCov::SourceFile.new(File.expand_path('spec/data/test/test.rb'), {"lines" => [1, 1, 1, 1, nil, 1, 1, 1, 1]})]
       expect(SimpleCov::ResultMerger).to receive_message_chain(:merged_result, :files).and_return(files)
       expect(runner.run).to eq([])
     end
